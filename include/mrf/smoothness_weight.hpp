d3ae5492e36e9a94a6cdaf0ba77e619d4abcc5eb
Sascha Wirges <sascha.wirges@kit.edu>
2016-12-19 10:12:26 +0100

    working now for units test "solver"

---
+++ include/mrf/smoothness_weight.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "pixel.hpp"
+
+namespace mrf {
+
+inline double smoothnessWeight(const Pixel& p, const Pixel& neighbor, const double& val_p,
+                               const double& val_neighbor) {
+    return exp(-abs(val_p - val_neighbor));
+}
+}
