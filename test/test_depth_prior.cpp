49f4f0cf818dc66090bb245be88c536e3be162f1
Bjoern Roxin <roxinbj@gmail.com>
2016-12-19 18:14:25 +0100

    test_depth_prior added

---
+++ test/test_depth_prior.cpp
@@ -0,0 +1,50 @@
+
+#include <boost/filesystem.hpp>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include <pcl/point_types.h>
+
+#include "camera_model_ortho.h"
+#include "depth_prior.hpp"
+#include "solver.hpp"
+#include "export.hpp"
+
+TEST(DepthPrior, Mean) {
+    using namespace mrf;
+
+    google::InitGoogleLogging("DepthPrior");
+    google::InstallFailureSignalHandler();
+    LOG(INFO) << "Depth prior Testing";
+
+    constexpr size_t rows = 10;
+    constexpr size_t cols = 10;
+    std::shared_ptr<CameraModelOrtho> cam{new CameraModelOrtho(cols, rows)};
+
+    using PointT = pcl::PointXYZ;
+    using DataT = Data<PointT>;
+    DataT::Image img{cv::Mat::eye(rows, cols, CV_32FC1)};
+    const DataT::Cloud::Ptr cl{new DataT::Cloud};
+    PointT p;
+    p.x = 1;
+    p.y = rows - 1;
+    p.z = 1;
+    cl->push_back(p);
+    p.x = cols - 1;
+    p.y = 1;
+    p.z = 0;
+    cl->push_back(p);
+    DataT d(cl, img, DataT::Transform::Identity());
+    Parameters params;
+    params.initialization = Initialization::meanDepth;
+    params.ks = 0;
+    params.kd =0;
+    LOG(INFO) << "params set to";
+    LOG(INFO) << params;
+    Solver solver{cam, params};
+    solver.solve(d);
+
+    boost::filesystem::path path_name{"/tmp/test/depthPrior/"};
+    boost::filesystem::create_directories(path_name);
+    exportData(d, path_name.string());
+    exportDepthImage<PointT>(d, cam, path_name.string());
+}
