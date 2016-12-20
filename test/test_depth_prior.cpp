c9c7c33582c76829896e5e43d39c28f6de23c26c
Bjoern Roxin <roxinbj@gmail.com>
2016-12-20 08:46:53 +0100

    prior in test

--- test/test_depth_prior.cpp
+++ test/test_depth_prior.cpp
@@ -6,8 +6,8 @@
 
 #include "camera_model_ortho.h"
 #include "depth_prior.hpp"
-#include "solver.hpp"
 #include "export.hpp"
+#include "solver.hpp"
 
 TEST(DepthPrior, Mean) {
     using namespace mrf;
@@ -16,8 +16,8 @@ TEST(DepthPrior, Mean) {
     google::InstallFailureSignalHandler();
     LOG(INFO) << "Depth prior Testing";
 
-    constexpr size_t rows = 10;
-    constexpr size_t cols = 10;
+    constexpr size_t rows = 15;
+    constexpr size_t cols = 15;
     std::shared_ptr<CameraModelOrtho> cam{new CameraModelOrtho(cols, rows)};
 
     using PointT = pcl::PointXYZ;
@@ -31,13 +31,33 @@ TEST(DepthPrior, Mean) {
     cl->push_back(p);
     p.x = cols - 1;
     p.y = 1;
-    p.z = 0;
+    p.z = 10;
     cl->push_back(p);
+    p.x = 2;
+    p.y = rows - 1;
+    p.z = 1;
+    cl->push_back(p);
+    p.x = 1;
+    p.y = rows - 1;
+    p.z = 1;
+    cl->push_back(p);
+    p.x = 1;
+    p.y = rows - 1;
+    p.z = 1;
+    cl->push_back(p);
+    p.x = 1;
+    p.y = rows - 1;
+    p.z = 1;
+    cl->push_back(p);
+    p.x = 1;
+    p.y = rows - 1;
+    p.z = 1;
+
     DataT d(cl, img, DataT::Transform::Identity());
     Parameters params;
-    params.initialization = Initialization::meanDepth;
+    params.initialization = Initialization::triangles;
     params.ks = 0;
-    params.kd =0;
+    params.kd = 0;
     LOG(INFO) << "params set to";
     LOG(INFO) << params;
     Solver solver{cam, params};
