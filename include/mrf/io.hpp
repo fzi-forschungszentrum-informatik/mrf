7195078b0ecd69752e7a937cafc5c269ed55e523
Sascha Wirges <sascha.wirges@kit.edu>
2017-01-28 12:59:11 +0100

    tidy up io, fix weights for scenenet

--- include/mrf/io.hpp
+++ include/mrf/io.hpp
@@ -22,48 +22,50 @@ inline cv::Mat createOutput(const cv::Mat& in, const bool normalize = true) {
     if (normalize) {
         cv::normalize(in, out, 0, std::numeric_limits<TargetT>::max(), cv::NORM_MINMAX);
         out.convertTo(out, cv::DataType<TargetT>::type);
-    } else {
+    } else
         in.convertTo(out, cv::DataType<TargetT>::type);
-    }
-
     return out;
+}
+
+inline void exportImage(const cv::Mat& img,
+                        const std::string& p,
+                        const bool normalize = true,
+                        const bool invert = false,
+                        const bool apply_color_map = false) {
+    const std::string file_name{p + "image.png"};
+    LOG(INFO) << "Writing image to '" << file_name << "'.";
+    cv::Mat out{createOutput(img, normalize)};
+    if (invert)
+        cv::bitwise_not(out, out);
+
+    if (apply_color_map)
+        cv::applyColorMap(out, out, cv::COLORMAP_HOT);
+
+    cv::imwrite(file_name, out);
+}
+
+template <typename T>
+inline void exportCloud(const typename pcl::PointCloud<T>::ConstPtr& cl, const std::string& p) {
+    if (!cl->empty()) {
+        const std::string file_name = p + "cloud.pcd";
+        LOG(INFO) << "Writing cloud to '" << file_name << "'.";
+        pcl::io::savePCDFile<T>(file_name, *cl, true);
+    }
 }
 
 template <typename T>
 void exportData(const Data<T>& d, const std::string& p, const bool normalize = true) {
-
-    std::string file_name;
-
-    /**
-     * Raw image
-     */
-    file_name = p + "image.png";
-    LOG(INFO) << "Writing image to '" << file_name << "'.";
-    cv::imwrite(file_name, createOutput(d.image, normalize));
-
-    /**
-     * Cloud
-     */
-    if (!d.cloud->empty()) {
-        file_name = p + "cloud.pcd";
-        LOG(INFO) << "Writing cloud to '" << file_name << "'.";
-        pcl::io::savePCDFile<T>(file_name, *(d.cloud), true);
-    }
-}
-
-inline bool inImageRange(const unsigned int width,
-                         const unsigned int height,
-                         const Eigen::Vector2i& p) {
-    return (p.x() >= 0) && (static_cast<unsigned int>(p.x()) < width) && (p.y() >= 0) &&
-           (static_cast<unsigned int>(p.y()) < height);
+    exportImage(createOutput(d.image, normalize), p);
+    exportCloud<T>(d.cloud, p);
 }
 
 template <typename T>
 void exportDepthImage(const Data<T>& d,
                       const std::shared_ptr<CameraModel>& cam,
                       const std::string& p,
-                      const bool normalize = true) {
-
+                      const bool normalize = true,
+                      const bool invert = false,
+                      const bool apply_color_map = false) {
     int rows, cols;
     cam->getImageSize(cols, rows);
     cv::Mat img_depth{cv::Mat::zeros(rows, cols, cv::DataType<double>::type)};
@@ -72,9 +74,9 @@ void exportDepthImage(const Data<T>& d,
     Eigen::Matrix2Xd img_pts_raw{Eigen::Matrix2Xd::Zero(2, pts_3d.cols())};
     const std::vector<bool> in_front{cam->getImagePoints(pts_3d, img_pts_raw)};
     for (size_t c = 0; c < in_front.size(); c++) {
-        if (!inImageRange(cols, rows, img_pts_raw.col(c).cast<int>()))
-            continue;
         const Pixel p{img_pts_raw(0, c), img_pts_raw(1, c)};
+        if (!p.inImage(rows, cols) || !in_front[c])
+            continue;
         Eigen::Vector3d support, direction;
         cam->getViewingRay(Eigen::Vector2d(p.col, p.row), support, direction);
         const Eigen::Hyperplane<double, 3> plane(direction, pts_3d.col(c));
@@ -83,31 +85,21 @@ void exportDepthImage(const Data<T>& d,
              support)
                 .norm();
     }
-
-    const std::string file_name{p + "depth.png"};
-    LOG(INFO) << "Writing image to '" << file_name << "'.";
-    cv::imwrite(file_name, createOutput(img_depth, normalize));
+    exportImage(
+        createOutput(img_depth, normalize), p + "depth_", normalize, invert, apply_color_map);
 }
 
-inline void exportImage(const cv::Mat& img, const std::string& p, const bool normalize = true) {
-    const std::string file_name{p + "image.png"};
-    LOG(INFO) << "Writing image to '" << file_name << "'.";
-    cv::imwrite(file_name, createOutput(img, normalize));
-}
 
 void exportResultInfo(const ResultInfo& info, const std::string& p) {
     if (info.has_covariance_depth) {
         cv::Mat out;
         cv::eigen2cv(info.covariance_depth, out);
-        cv::imwrite(p + "covariance_depth.png", createOutput(out));
+        exportImage(createOutput(out), p + "covariance_depth_");
     }
     if (info.has_weights) {
         cv::Mat out;
         cv::eigen2cv(info.weights, out);
-        out = createOutput(out);
-        cv::bitwise_not(out, out);
-        cv::applyColorMap(out, out, cv::COLORMAP_HOT);
-        cv::imwrite(p + "weights.png", out);
+        exportImage(createOutput(out), p + "weights_", true, false, true);
     }
 }
 }
