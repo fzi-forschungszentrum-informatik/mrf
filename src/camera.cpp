553dd5eb48dcb659f0297ff7a6387679863c90b2
Matthias Mayr <matthias.mayr@student.kit.edu>
2017-03-29 15:21:27 +0200

    Removed clone function from camera.h

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,13 +1,5 @@
 #include "camera.h"
 
-// Camera Camera::clone() {
-//	Camera camOut;
-//	camOut.referenceFrame = this->referenceFrame;
-//	camOut.cameraPose = this->cameraPose;
-//	camOut.cameraModel = this->cameraModel->clone();
-//	return camOut;
-//}
-
 std::ostream& operator<<(std::ostream& out, Camera& cam) {
     out << *cam.cameraModel.get();
     out << "reference frame: " << cam.referenceFrame << std::endl;
